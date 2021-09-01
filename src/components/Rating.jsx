ae0ce41c47726d9d941d931b8de5020dbf3adc76
AnaPaulaBraga <anapaula.junq@hotmail.com>
2021-09-01 16:39:33 -0300

    requisitos 11 e 12

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-class Rating extends React.Component{
-    render(){
-        return()
-    }
+class Rating extends React.Component {
+  render() {
+    return (
+      <div>
+        { rating }
+      </div>
+
+    );
+  }
 }
 
 export default Rating;
