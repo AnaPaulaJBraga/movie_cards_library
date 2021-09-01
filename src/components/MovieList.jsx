e8d489b9496cc95e85e0868504c2ab191e62eb74
AnaPaulaBraga <anapaula.junq@hotmail.com>
2021-09-01 16:37:06 -0300

    requisitos 7 a 10

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (
-      <div className="MovieList" />
+      <div className="MovieList">
+        {movies.map((movie) => <MovieCard key={ movie.title } />)}
+      </div>
 
     );
   }
